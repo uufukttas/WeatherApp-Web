a00cac69e8fb152504434b9e81ac742aa83465b5
UFUK TAS <uufukttas@hotmail.com>
2023-12-10 00:14:21 +0300

    updated useeffect function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,12 @@
-import React, { FormEvent, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { FormEvent, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { setWeather } from './weatherSlice';
 
 const Form = () => {
     const REQUEST_URL = 'http://api.openweathermap.org';
     const dispatch = useDispatch();
-    const getWeatherData = async (city: string) => {
+
+    const getWeatherData = useCallback(async (city: string) => {
         try {
             const location = await getLocationData(city);
             const response = await fetch(`${REQUEST_URL}/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${process.env.REACT_APP_WEATHER_APP_ACCESS_KEY}`);
@@ -17,12 +18,14 @@ const Form = () => {
         } catch (error) {
             console.log(error);
         }
-    };
+    }, [dispatch]);
+
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         await getWeatherData(event.target[0].value);
     };
+
     const getLocationData = async (city: string) => {
         const response = await fetch(`${REQUEST_URL}/geo/1.0/direct?q=${city}&appid=${process.env.REACT_APP_WEATHER_APP_ACCESS_KEY}`);
         const data = await response.json();
@@ -34,9 +37,9 @@ const Form = () => {
         const fetchData = async () => {
             await getWeatherData('London');
         };
-    
+
         fetchData();
-    }, [getWeatherData]); 
+    }, [getWeatherData]);
 
     return (
         <div className="max-w-sm mx-auto bg-white rounded p-6 shadow-md w-full">
@@ -53,7 +56,7 @@ const Form = () => {
                 />
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default Form;
